066d6ec16d0a7f8f29a4dbee1644a6544b192082
Julianne Gonski <jtgonski@gmail.com>
2017-09-27 09:00:41 -0400

    basic skeleton of HTML page, routes linked

---
+++ app/data/friends.js
@@ -0,0 +1,74 @@
+var friendsArray = [
+	{
+		"name": "Phillipe Dixon", 
+		"photo": "http://3.bp.blogspot.com/-bJ1cD3ReHBo/T6v5BWiBLaI/AAAAAAAAFkM/4HRdqLLn2wk/s1600/xprs-hulk.jpg", 
+		"scores": [
+			"5", 
+			"1",
+			"5", 
+			"1", 
+			"1", 
+			"1", 
+			"3", 
+			"2", 
+			"3", 
+			"5"
+		]
+	}, 
+	{
+		"name" : "Julian Bridges", 
+		"photo": "http://images5.fanpop.com/image/photos/30100000/Donkey-shrek-30165006-445-445.jpg", 
+		"scores": [
+			"5", 
+			"5", 
+			"5", 
+			"3", 
+			"1", 
+			"4", 
+			"1", 
+			"4", 
+			"2", 
+			"1"
+		]
+	}, 
+	{
+		"name": "Julianne Gonski", 
+		"photo": "http://media-cache-ak0.pinimg.com/236x/7d/4f/c2/7d4fc24ad5e4c717db2ce951423fa39b.jpg", 
+		"scores": [
+			"2", 
+			"3", 
+			"2", 
+			"5", 
+			"5", 
+			"2", 
+			"2", 
+			"4", 
+			"5", 
+			"5"
+		]
+	},
+	{
+		"name": "Shrek", 
+		"photo": "http://s3.firstpost.in/wp-content/uploads/2014/02/shrek_380.gif"
+		"scores": [
+			"1", 
+			"5", 
+			"5", 
+			"1", 
+			"5", 
+			"1", 
+			"5", 
+			"1", 
+			"1", 
+			"5"
+		]
+	}
+];
+
+module.exports = friendsArray; 
+
+
+
+
+
+
